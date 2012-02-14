afd2415ed47e05e5aebe0ba8bd5a50460fbbea5c
k4sbasia <k4sbasia@gmail.com>
2012-02-14 08:14:13 +0000

    21 tutorial

---
+++ The New Boston/src/com/k4sbasia/Splash.java
@@ -0,0 +1,45 @@
+package com.k4sbasia;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+	
+	MediaPlayer ourSong;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		
+		ourSong = MediaPlayer.create(Splash.this, R.raw.splash);
+		ourSong.start();
+		
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(10000);
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				} finally {
+					Intent openStartingPoint = new Intent("com.k4sbasia.MENU");
+					startActivity(openStartingPoint);
+				}
+			}
+		};
+		
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		ourSong.release();
+		finish();
+	}
+
+}
--- The New Boston/src/com/k4sbasia/TheNewBostonActivity.java
+++ The New Boston/src/com/k4sbasia/TheNewBostonActivity.java
@@ -1,6 +1,7 @@
 package com.k4sbasia;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,7 @@ import android.widget.TextView;
 public class TheNewBostonActivity extends Activity {
 	
 	int counter;
-	Button add, sub;
+	Button add, sub, menu;
 	TextView display;
 	
     /** Called when the activity is first created. */
@@ -21,6 +22,8 @@ public class TheNewBostonActivity extends Activity {
         counter = 0;
         add = (Button) findViewById(R.id.bAdd);
         sub = (Button) findViewById(R.id.bSub);
+        menu = (Button) findViewById(R.id.bMenu);
+        
         display = (TextView) findViewById(R.id.tvTotal);
         
         add.setOnClickListener(new View.OnClickListener() {
@@ -40,5 +43,24 @@ public class TheNewBostonActivity extends Activity {
 				
 			}
 		});
+        
+        menu.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent openStartingPoint = new Intent("com.k4sbasia.MENU");
+				startActivity(openStartingPoint);
+			}
+		});
      }
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+    
+    
 }
