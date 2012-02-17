62df3e8945cfc9cf107149b1ed6c6a432ba0d9c1
k4sbasia <k4sbasia@gmail.com>
2012-02-17 00:05:10 +0000

    add tutorial 54

---
+++ The New Boston/src/com/k4sbasia/AboutUs.java
@@ -0,0 +1,13 @@
+package com.k4sbasia;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutUs extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceSate) {
+		super.onCreate(savedInstanceSate);
+		setContentView(R.layout.about);
+	}
+}
--- The New Boston/src/com/k4sbasia/Menu.java
+++ The New Boston/src/com/k4sbasia/Menu.java
@@ -3,22 +3,25 @@ package com.k4sbasia;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class Menu extends ListActivity {
-	
-	String classes[] = {"TheNewBostonActivity", "Text", "HelloWorldActivity", "Email", "Camera", "Data", "Splash"};
+
+	String classes[] = { "TheNewBostonActivity", "Text", "HelloWorldActivity",
+			"Email", "Camera", "Data", "Splash" };
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		// TODO Auto-generated method stub
-		
+
 		super.onListItemClick(l, v, position, id);
-		
+
 		String menuItem = classes[position];
-		
+
 		try {
 			Class ourClass = Class.forName("com.k4sbasia." + menuItem);
 			Intent intent = new Intent(Menu.this, ourClass);
@@ -34,9 +37,36 @@ public class Menu extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
-		setListAdapter(new ArrayAdapter<String>(Menu.this, android.R.layout.simple_list_item_1, classes));
+
+		setListAdapter(new ArrayAdapter<String>(Menu.this,
+				android.R.layout.simple_list_item_1, classes));
 	}
 
-	
+	@Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		// TODO Auto-generated method stub
+		super.onCreateOptionsMenu(menu);
+		MenuInflater blowUp = getMenuInflater();
+		blowUp.inflate(R.menu.cool_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+
+		switch (item.getItemId()) {
+		case R.id.aboutUs:
+			Intent i = new Intent("com.k4sbasia.ABOUT");
+			startActivity(i);
+			
+			break;
+
+		case R.id.preferences:
+			
+			break;
+		}
+		return false;
+	}
+
 }
