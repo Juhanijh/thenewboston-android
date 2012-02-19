38fb0767a6816fe08a42ec184d5c4c973ff0ea29
k4sbasia <k4sbasia@gmail.com>
2012-02-19 20:23:35 +0000

    add update, delete SQLite DB

--- The New Boston/src/com/k4sbasia/HotOrNot.java
+++ The New Boston/src/com/k4sbasia/HotOrNot.java
@@ -2,6 +2,8 @@ package com.k4sbasia;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -53,10 +55,68 @@ public class HotOrNot {
 		}
 	}
 
-	public long createEntry(String name, String hostness) {
+	public long createEntry(String name, String hostness) throws SQLException {
 		ContentValues cv = new ContentValues();
 		cv.put(KEY_NAME, name);
 		cv.put(KEY_HOTNESS, hostness);
 		return ourDatabase.insert(DATABASE_TABLE, null, cv);
 	}
+
+	public String getData() throws SQLException {
+		String[] columns = new String[] { KEY_ROWID, KEY_NAME, KEY_HOTNESS };
+		Cursor c = ourDatabase.query(DATABASE_TABLE, columns, null, null, null,
+				null, null);
+
+		String result = "";
+
+		int iRow = c.getColumnIndex(KEY_ROWID);
+		int iName = c.getColumnIndex(KEY_NAME);
+		int iHotness = c.getColumnIndex(KEY_HOTNESS);
+
+		for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+			result = result + c.getString(iRow) + " " + c.getString(iName)
+					+ " " + c.getString(iHotness) + "\n";
+		}
+		return result;
+	}
+
+	public String getName(long l) throws SQLException {
+		String[] columns = new String[] { KEY_ROWID, KEY_NAME, KEY_HOTNESS };
+		Cursor c = ourDatabase.query(DATABASE_TABLE, columns, KEY_ROWID + "="
+				+ l, null, null, null, null);
+		if (c != null) {
+			c.moveToFirst();
+			String name = c.getString(1);
+			return name;
+		}
+		return null;
+	}
+
+	public String getHotness(long l) throws SQLException {
+		String[] columns = new String[] { KEY_ROWID, KEY_NAME, KEY_HOTNESS };
+		Cursor c = ourDatabase.query(DATABASE_TABLE, columns, KEY_ROWID + "="
+				+ l, null, null, null, null);
+		if (c != null) {
+			c.moveToFirst();
+			String hotness = c.getString(2);
+			return hotness;
+		}
+		return null;
+	}
+
+	public void updateEntry(long lRow, String mName, String mHotness)
+			throws SQLException {
+		String[] columns = new String[] { KEY_ROWID, KEY_NAME, KEY_HOTNESS };
+		ContentValues cvUpdate = new ContentValues();
+		cvUpdate.put(KEY_NAME, mName);
+		cvUpdate.put(KEY_HOTNESS, mHotness);
+
+		ourDatabase.update(DATABASE_TABLE, cvUpdate, KEY_ROWID + "=" + lRow,
+				null);
+	}
+
+	public void deleteEntry(long lRowDelete) throws SQLException {
+		ourDatabase.delete(DATABASE_TABLE, KEY_ROWID + "=" + lRowDelete, null);
+	}
+
 }
--- The New Boston/src/com/k4sbasia/SQLiteExample.java
+++ The New Boston/src/com/k4sbasia/SQLiteExample.java
@@ -1,16 +1,20 @@
 package com.k4sbasia;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SQLiteExample extends Activity implements OnClickListener {
 
-	Button sqlUpdate, sqlView;
-	EditText sqlName, sqlHotness;
+	Button sqlUpdate, sqlView, sqlModify, sqlGetInfo, sqlDelete;
+	EditText sqlName, sqlHotness, sqlRow;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +28,131 @@ public class SQLiteExample extends Activity implements OnClickListener {
 		sqlView = (Button) findViewById(R.id.bSQLopenView);
 		sqlView.setOnClickListener(this);
 		sqlUpdate.setOnClickListener(this);
+
+		sqlRow = (EditText) findViewById(R.id.etSQLrowInfo);
+		sqlModify = (Button) findViewById(R.id.bSQLmodify);
+		sqlGetInfo = (Button) findViewById(R.id.bGetInfo);
+		sqlDelete = (Button) findViewById(R.id.bSQLdelete);
+
+		sqlDelete.setOnClickListener(this);
+		sqlModify.setOnClickListener(this);
+		sqlGetInfo.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.bSQLUpdate:
-			String name = sqlName.getText().toString();
-			String hostness = sqlHotness.getText().toString();
+			String name = null;
+			String hotness = null;
 
-			HotOrNot entry = new HotOrNot(SQLiteExample.this);
-			entry.open();
-			entry.createEntry(name, hostness);
-			entry.close();
+			boolean didItWork = true;
+			try {
+				name = sqlName.getText().toString();
+				hotness = sqlHotness.getText().toString();
+
+				HotOrNot entry = new HotOrNot(SQLiteExample.this);
+				entry.open();
+				entry.createEntry(name, hotness);
+				entry.close();
+			} catch (Exception e) {
+				didItWork = false;
+				String error = e.toString();
+				Dialog d = new Dialog(this);
+				d.setTitle("Dang it!");
+				TextView tv = new TextView(this);
+				tv.setText(error);
+				d.setContentView(tv);
+				d.show();
+				e.printStackTrace();
+			} finally {
+				if (didItWork) {
+					// Dialog
+					// Dialog d = new Dialog(this);
+					// d.setTitle("Heck Yea!");
+					// TextView tv = new TextView(this);
+					// tv.setText("Success");
+					// d.setContentView(tv);
+					// d.show();
+					Toast.makeText(getApplicationContext(),
+							"Heck Yea!" + name + " - " + hotness + " saved",
+							Toast.LENGTH_SHORT).show();
+				}
+			}
 			break;
 
 		case R.id.bSQLopenView:
+			Intent i = new Intent("com.k4sbasia.SQLVIEW");
+			startActivity(i);
+			break;
+
+		case R.id.bGetInfo:
+			String s = sqlRow.getText().toString();
+			long l = Long.parseLong(s);
+			HotOrNot hon = new HotOrNot(this);
+			hon.open();
+			try {
+				String returnedName = hon.getName(l);
+				String returnedHotness = hon.getHotness(l);
+				hon.close();
+				sqlName.setText(returnedName);
+				sqlHotness.setText(returnedHotness);
+			} catch (Exception e) {
+				String error = e.toString();
+				Dialog d = new Dialog(this);
+				d.setTitle("Dang it!");
+				TextView tv = new TextView(this);
+				tv.setText(error);
+				d.setContentView(tv);
+				d.show();
+				e.printStackTrace();
+			}
+			break;
+		case R.id.bSQLmodify:
+			String mName = sqlName.getText().toString();
+			String mHotness = sqlHotness.getText().toString();
+			String sRow = sqlRow.getText().toString();
+			long lRow = Long.parseLong(sRow);
+
+			HotOrNot ex = new HotOrNot(this);
+			ex.open();
+			try {
+				ex.updateEntry(lRow, mName, mHotness);
+			} catch (Exception e) {
+				String error = e.toString();
+				Dialog d = new Dialog(this);
+				d.setTitle("Dang it!");
+				TextView tv = new TextView(this);
+				tv.setText(error);
+				d.setContentView(tv);
+				d.show();
+				e.printStackTrace();
+			}
+
+			ex.close();
+
+			break;
+
+		case R.id.bSQLdelete:
+			String sRowDelete = sqlRow.getText().toString();
+			long lRowDelete = Long.parseLong(sRowDelete);
+			HotOrNot delete = new HotOrNot(this);
+			delete.open();
+			try {
+				delete.deleteEntry(lRowDelete);
+			} catch (Exception e) {
+				String error = e.toString();
+				Dialog d = new Dialog(this);
+				d.setTitle("Dang it!");
+				TextView tv = new TextView(this);
+				tv.setText(error);
+				d.setContentView(tv);
+				d.show();
+				e.printStackTrace();
+			}
+
+			delete.close();
+
 			break;
 		}
 	}
